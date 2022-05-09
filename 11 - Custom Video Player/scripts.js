e8b4e5ebb39dd5688f52a516344bba5640d17834
Weian Wang <stargazer.wang@gmail.com>
2022-05-09 14:18:27 +0800

    Update scripts.js
    done lesson 11

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -13,14 +13,58 @@ function togglePlay() {
 
 
 function updateButton() {
-    console.log(this);
-    console.log(this.paused);
+    const icon = this.paused ? '►' : '❚ ❚';
+    toggle.textContent = icon;
 }
 
 
+function skip() {
+    video.currentTime += parseFloat(this.dataset.skip);
+}
+
+function handleRangeUpdate() {
+    video[this.name] = this.value;
+}
+
+
+function handleProgress() {
+    const percent = (video.currentTime / video.duration)*100;
+    progressBar.style.flexBasis = `${percent}%`;
+}
+
+
+function scrub(e) {
+    const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+    video.currentTime = scrubTime;
+}
+
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+video.addEventListener('timeupdate', handleProgress);
 toggle.addEventListener('click', togglePlay);
 document.addEventListener('keydown', (e) => {
     if (e.code === "Space") togglePlay(e)
 })
+
+
+skipButtons.forEach(btn => 
+    btn.addEventListener('click', skip) 
+)
+
+ranges.forEach(range => range.addEventListener('change', handleRangeUpdate))
+progress.addEventListener('click', scrub);
+progress.addEventListener('mousemove', (e) => {
+    mouseDown && scrub(e)
+});
+
+
+let mouseDown = false;
+progress.addEventListener('mousedown', () => {
+    mouseDown = true;
+})
+
+progress.addEventListener('mouseup', () => {
+    mouseDown = false;
+})
+
